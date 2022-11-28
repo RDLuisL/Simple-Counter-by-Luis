533867899b4c5c6f10daa91871f9272bbba4ab0b
LuisL <luislunandroid@gmail.com>
2022-11-28 22:59:25 +0000

    version revisada para entregar, con fondo de chronografo pero luego de aprender useEffect y useState lo hubiera hecho con estos para que sirviera como chronografo real

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
-import SimpleCounter from "./counter.js";
+import SimpleCounter from "./counter";
 
 //create your first component
 const Home = () => {
--- src/js/index.js
+++ src/js/index.js
@@ -9,4 +9,55 @@ import "../styles/index.css";
 import Home from "./component/home.jsx";
 
 //render your react application
-ReactDOM.render(<Home />, document.querySelector("#app"));
+import PropTypes from "prop-types";
+
+function SimpleCounter(props){
+
+    return (
+        <div class="">
+                <div class="position-absolute">
+                    <img src="https://imagizer.imageshack.com/img923/41/j5nap6.png" className="position-absolute" alt="Stop Watch image"></img>
+                
+                    <div className= "bigCounter position-relative">
+                        <div className="four e-hours">{props.digiFour}</div>
+                        <div className="three hours">{props.digiThree}</div>
+                        <div className="two minute">{props.digiTwo}</div>
+                        <div className="one sec">{props.digiOne}</div>
+                    </div> 
+                </div>
+        
+        </div>
+        
+        
+    );
+    
+};
+
+SimpleCounter.propTypes ={
+    digiFour: PropTypes.number,
+    digiThree: PropTypes.number,
+    digiTwo: PropTypes.number,
+    digiOne: PropTypes.number
+ };
+
+let counter = 0;
+setInterval (function(){
+    const four = Math.floor(counter/10000)
+    const three = Math.floor(counter/3600)
+    const two = Math.floor(counter/59)
+    const one = Math.floor(counter/1)
+
+    console.log(counter)
+    counter++
+
+
+
+
+
+ReactDOM.render(
+    <SimpleCounter digiOne={"0"+one} digiTwo={two} digiThree={three} digiFour={four} />,
+    document.querySelector("#app")
+    
+);
+}, 1000);
+export default SimpleCounter;
